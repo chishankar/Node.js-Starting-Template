236159f82c0bcb049353a97134f49e630df80036
Chirag Shankar <chishankar@gmail.com>
2018-05-15 10:52:26 -0400

    Equipped with Mongoose and Socket.io

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ var _ = require("underscore");
 var app = express();
 var PORT = 3000;
 
+// For using MongoDB
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+dotenv.load();
+
+// Initializing/connecting the MongoDB database variable
+mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
+
+// Setting up Socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 /// Setup body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,10 +44,15 @@ app.delete("ENTER_END_POINT_HERE", function(req, res) {
     res.send("UNIMPLEMENTED ENDPOINT");
 });
 
-// Start listening on port PORT
-app.listen(PORT, function() {
-    console.log('Server listening on port:', PORT);
+
+// io on connection
+io.on('connection', function(socket) {
+    console.log("Connected!");
+    console.log(Object.keys(io.sockets.sockets).length);
 });
 
-// DO NOT REMOVE
-exports.PORT = PORT;
+// Start listening on port PORT
+http.listen(3000, function() {
+    console.log('Welcome Express.js template. Equiped with: ');
+    console.log("[Mongoose, Socket.io, Underscore]");
+});
